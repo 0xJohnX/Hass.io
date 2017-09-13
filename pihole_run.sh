7fd864aa0a4edfbcf7db090bba57c0fd75cc0288
Junlong <zhaojunll1987@gmail.com>
2017-09-13 23:03:06 +0800

    Update pihole_run.sh

--- pihole_run.sh
+++ pihole_run.sh
@@ -3,7 +3,6 @@ IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')
 IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
 IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
 DOCKER_CONFIGS="$(pwd)"  # Default of directory you run this from, update to where ever.
-
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp -p 80:80 \
