2a54a519873c78278e1e1c1bab9636f5c1fbd9d1
Junlong <zhaojunll1987@gmail.com>
2017-09-13 22:56:35 +0800

    Add files via upload

---
+++ pihole_run.sh
@@ -0,0 +1,16 @@
+IP_LOOKUP="$(ip route get 114.114.114.114 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')"  # May not work for VPN / tun0
+IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
+IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
+DOCKER_CONFIGS="$(pwd)"  # Default of directory you run this from, update to where ever.
+
+docker run -d \
+    --name pihole \
+    -p 53:53/tcp -p 53:53/udp -p 80:80 \
+    -v "${DOCKER_CONFIGS}/pihole/:/etc/pihole/" \
+    -v "${DOCKER_CONFIGS}/dnsmasq.d/:/etc/dnsmasq.d/" \
+    -e ServerIP="${IP}" \
+    -e ServerIPv6="${IPv6}" \
+    -e TZ='Asia/Shanghai'
+    --restart=always \
+    diginc/pi-hole:alpine
